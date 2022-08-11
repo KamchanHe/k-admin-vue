22b367c5969823782538ea1f17a1ed4f9f084140
KamchanHe <47138387+KamchanHe@users.noreply.github.com>
2022-08-11 23:36:57 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import 'normalize.css/normalize.css';
+import '@/styles/index.scss';
+
+// eslint-disable-next-line import/no-unresolved
+import 'virtual:svg-icons-register';
+
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteEslint from 'vite-plugin-eslint';
+import legacy from '@vitejs/plugin-legacy';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { resolve } from 'path';
+
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+import { visualizer } from 'rollup-plugin-visualizer';
+import { viteMockServe } from 'vite-plugin-mock';
 
 export default defineConfig({
-  plugins: [vue(), viteEslint()]
+  plugins: [
+    vue(),
+    viteEslint(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({ resolvers: [ElementPlusResolver()] }),
+    vueJsx(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    }),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [resolve('./src/assets/icons/')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]'
+    }),
+    viteMockServe({
+      mockPath: '/mock',
+      localEnabled: true
+    }),
+    visualizer({ open: true })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('./src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";'
+      }
+    }
+  }
 });
